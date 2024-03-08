5da2ad613958e0eea00bbac40dd9a45b10d78c93
Yvan-David <irankundayvan2020@gmail.com>
2024-03-08 09:16:18 +0200

    add port option from env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,14 +5,12 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-import {version} from '../package.json';
 import swaggerDocs from '../src/utils/swagger';
 
 import router from './router';
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(cors({
     credentials: true,
@@ -25,35 +23,9 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-/* const options: swaggerJsdoc.Options = {
-    definition: {
-        openapi: "3.0.0",
-        info:{
-            title: 'REST API DOCS',
-            version,
-            description:
-                "This is my portfolio api application documented with swagger"
-        },
-        servers: [
-            {
-                url: "http://localhost:8080/",
-            },
-        ],
-    },
-    apis: ['./router/index.ts'],
-};
-
-
-const spacs = swaggerJsdoc(options);
-app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(spacs)
-) */
-
 const server = http.createServer(app);
 
-server.listen(8080, "0.0.0.0" , () => {
+server.listen(port , () => {
     console.log('Server running on http://localhost:8080/');
     swaggerDocs(app, 8080)
 });
